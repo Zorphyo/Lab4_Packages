db53e00846127c9445421c6b055775385c613f08
Zorphyo <zorphyo@outlook.com>
2024-09-25 04:04:50 -0400

    Added and Implemented New Input System and MP4 Support

--- Packages/Assets/Scripts/PlayerControls.cs
+++ Packages/Assets/Scripts/PlayerControls.cs
@@ -1,34 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Callbacks;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerControls : MonoBehaviour
 {
     public GameObject laserPrefab;
+    public PlayerActionInputs playerMovement;
+    public Rigidbody2D rb;
 
+    Vector2 moveDirection;
+    private InputAction move;
+    private InputAction fire;
     private float speed = 6f;
     private float horizontalScreenLimit = 10f;
     private float verticalScreenLimit = 6f;
-    private bool canShoot = true;
-
     AudioSource audioSource;
+
+    void Awake()
+    {
+        playerMovement = new PlayerActionInputs();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Movement();
-        Shooting();
-    }
+        moveDirection = move.ReadValue<Vector2>();
 
-    void Movement()
-    {
-        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * Time.deltaTime * speed);
         if (transform.position.x > horizontalScreenLimit || transform.position.x <= -horizontalScreenLimit)
         {
             transform.position = new Vector3(transform.position.x * -1f, transform.position.y, 0);
@@ -39,20 +44,30 @@ public class PlayerControls : MonoBehaviour
         }
     }
 
-    void Shooting()
+     private void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && canShoot)
-        {
-            Instantiate(laserPrefab, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
-            audioSource.Play();
-            canShoot = false;
-            StartCoroutine("Cooldown");
-        }
+        rb.velocity = new Vector2(moveDirection.x * speed, moveDirection.y * speed);
     }
 
-    private IEnumerator Cooldown()
+    private void OnEnable()
     {
-        yield return new WaitForSeconds(1f);
-        canShoot = true;
+        move = playerMovement.Player.Move;
+        move.Enable();
+
+        fire = playerMovement.Player.Fire;
+        fire.Enable();
+        fire.performed += Fire;
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+        fire.Disable();
+    }
+
+    void Fire(InputAction.CallbackContext context)
+    {
+        Instantiate(laserPrefab, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
+        audioSource.Play();
     }
 }
